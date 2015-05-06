9f8e16127ec2905a1cca1f721bf88dedeb3bcca0
Inzamam <izzyrahaman@gmail.com>
2015-05-06 16:53:54 -0400

    Added code for different number formats

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
 import Data.List
+import Numeric
 
 -- data type to represent abstract syntax tree
 data LispVal = Atom String
@@ -24,6 +25,12 @@ instance Show LispVal where
   show (DottedList xs _) = "(" ++  unwords (map show xs) ++ ")"
   show (Character ch) = show ch
   show (Float d) = show d
+
+
+-- used to fashion readHex, readOct ,and readBin
+readUsingFun fun = fst . head . fun
+
+
 
 escapes :: Parser Char
 escapes = do
@@ -63,13 +70,57 @@ parseAtom = do
                          "#f" -> Bool False
                          _    -> Atom atom
 
+parseDecimal :: Parser LispVal
+parseDecimal = liftM (Number . read) $ many1 digit
+
+parseOctal :: Parser LispVal
+parseOctal = do
+              digits <- many1 (oneOf "01234567")
+              return $ Number $ readUsingFun readOct digits
+
+parseHex :: Parser LispVal
+parseHex = do
+            digits <- many1 (oneOf "0123456789ABCDEFabcdef")
+            return $ Number $ readUsingFun readHex digits
+
+binaryToInt :: String -> Integer
+binaryToInt = foldl (\x y -> x * 2 + (read (show y))) 0
+
+parseBinary :: Parser LispVal
+parseBinary = do
+                digits <- many1 (oneOf "01")
+                return $ Number $ binaryToInt digits
+
+
+
+parseRadix :: Parser LispVal
+parseRadix = do
+              char '#'
+              ch <- oneOf "bodx"
+              case ch of
+                'd' -> parseDecimal
+                'o' -> parseOctal
+                'b' -> parseBinary
+                'x' -> parseHex
+
+parseFloat :: Parser LispVal
+parseFloat = do
+                start <- many1 digit
+                dot <- char '.'
+                rest <- many1 (digit <|> char 'L')
+                let num = rd (start ++ (dot:rest))
+                return $ Float num
+                where
+                  rd = read :: String -> Double
+
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = parseRadix <|> parseDecimal
 
 parseExpr :: Parser LispVal
 parseExpr = try parseCharacter
           <|> parseAtom
           <|> parseString
+          <|> try parseFloat
           <|> parseNumber
 
 symbol :: Parser Char
